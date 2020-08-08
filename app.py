c0df778eee09bdfdc160937d4e169ae246c6634b
advaithm <advaith.madhukar@gmail.com>
2020-08-08 12:02:03 +0530

    automating update & deployment

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+import urllib.parse
+import os
+import threading
+
+app = Flask(__name__)
+
+
+def update(url):
+    os.system(f"python gen_new.py --url {url}")
+
+
+@app.route("/")
+def update():
+    url = request.get_data("url")
+    url = url.decode("utf-8")
+    url = urllib.parse.unquote(url)
+    url = url.split("=")
+    url = url[1]
+    print(url)
+    update_thread = threading.Thread(target=update, args=url)
+    update_thread.start()
+    return "hello"
---
+++ gen_new.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3.8
+import json
+import requests
+import argparse
+import hashlib
+
+
+def reconstruct(url):
+    # opens file with op handler in read mode
+    r = requests.get(url, allow_redirects=True)
+    open("nuclear.tar.gz", "wb").write(r.content)
+    sha256 = hashlib.sha256()
+    with open("nuclear.tar.gz", "rb") as f:
+        for byte_block in iter(lambda: f.read(4096), b""):
+            sha256.update(byte_block)
+        f.close()
+    shahash = sha256.hexdigest()
+    with open("org.js.nuclear.Nuclear.json") as f:
+        print("loading data")
+        # loading json as dict
+        data = json.load(f)
+        # closing the file handler b/c it looks the file
+        f.close()
+        print("Modifying data")
+        # updates the url value. the wierd formating is due to python loading the file
+        # using python data types
+        data["modules"][0]["sources"][0]["url"] = url
+        data["modules"][0]["sources"][0]["sha256"] = shahash
+        print("done")
+    # opens file in write mode
+    with open("org.js.nuclear.Nuclear.json", "w") as f:
+        print("updating json")
+        # formates data with some pretty printing
+        formated_data = json.dumps(data, indent=4, sort_keys=True)
+        # writes data to the file
+        f.writelines(formated_data)
+        # close file handler b/c best pratices
+        f.close()
+        print("done")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--url", type=str, help="url to replace.")
+    args = parser.parse_args()
+    if args != None:
+        reconstruct(args.url)
+    else:
+        print("No arguments give requires Url to continue")
+
+
+if __name__ == "__main__":
+    main()
